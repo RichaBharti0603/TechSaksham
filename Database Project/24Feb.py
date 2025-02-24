b24bed08e0746a644a4ed1aad17f112dd558e716
RichaBharti0603 <richab820@gmail.com>
2025-02-24 11:40:54 +0530

    Created Menu based Student Management System with SQL database

--- Database Project/24Feb.py
+++ Database Project/24Feb.py
@@ -76,11 +76,53 @@ def signin(data):
     except Exception as e:
         print("Error:", e)
         return False  # Return False if there's an error
+    
+def search(roll):
+    try:
+        # Connect to database using environment variables
+        con = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        
+        cur = con.cursor()
+        cur.execute(f"select roll, name, branch, admin_year, per_10, per_12 from student_record where roll = {roll}")
+        result = cur.fetchone()
+        if result!= None:
+            con.close()
+            return result
+        else:
+            con.close()
+            return ()
+    except Exception as e:
+        print(e)
+
+def displayall():
+    try:
+        # Connect to database using environment variables
+        con = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        
+        cur = con.cursor()
+        cur.execute(f"SELECT *FROM student_record")
+        return cur.fetchall()
+        
+    except Exception as e:
+        print(e)
+
 
 print("*"*20 +"Welcome to My App" + "*"*20)
 while True:
     print("1. SignIn \n  2. SignUp \n 3. Search 4. DisplayAll \n 5.Exit ")
     ch= int (input("Enter Your Choice"))
+
+
     if ch==1:
         roll = int(input("Enter Your Roll Number"))
         password= int(input("Enter your password"))
@@ -89,6 +131,8 @@ while True:
             print("You are successfully Login")
         else:
             print("Invalid Login or Password")
+
+
     elif ch==2:
         roll = int(input("Enter Your Roll Number:"))
         name = input("Enter Your Name")
@@ -101,10 +145,21 @@ while True:
             print("Student Registered")
         else:
             print("Student already Registered")
+
+
     elif ch==3:
-        pass
+        roll = int(input("Enter roll number to search"))
+        info = search(roll)
+        print(info)
+
+
     elif ch==4:
-        pass
+        allrecord = displayall()
+        for record in allrecord:
+            print(f"{record[0]} \t {record[1]} \t {record[2]} \t {record[3]} \t {record[4]}")
+        print("*"*20)
+
+
     elif ch==5:
         break
 
